3b22c07a7eb43f850d1af789e7de982f3ddab5e0
HappyJayXin <36494560+HappyJayXin@users.noreply.github.com>
2018-04-21 17:13:49 +0800

    newapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Testapp';
+  title:string="學習小幫手";
+  show=true;
+  ps:number;
+  fib:Array<number>=[];
+  result:string;
+  constructor() {     
+  }     
+  ngOnInit() {      
+    this.fib[1]=3;
+    this.fib[2]=4;
+    for(var i=3;i<=20;i++){
+      this.fib[i]=this.fib[i-1]+this.fib[i-2];
+    }           
+  }
+  ok(){
+    if(this.ps==this.fib[12]+this.fib[18]){
+      //this.show=false;
+      this.result=''; 
+      $('.text').slideDown(3000);     
+    }else{     
+      this.result='密碼錯誤';
+    }
+  } 
 }
